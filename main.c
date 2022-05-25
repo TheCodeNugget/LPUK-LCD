545b002be15820a7256b13224addf3eb16db279c
KenTheNugget <95768094+TheCodeNugget@users.noreply.github.com>
2022-05-25 22:31:07 +0300

    Example Added

--- main.c
+++ main.c
@@ -17,11 +17,13 @@ int main(void) { // TODO: Delete This Comment
 
     initLCD();
 
-    cursorDown();
     int k;
-    char input[40]= "---------------E-------E";
-    for(k=0;k<strlen(input);k++) writedata(input[k],1);
-    scrollLeft();
+    char line1[10]= "PEM - 1175";
+    for(k=0;k<sizeof(line1);k++) writedata(line1[k],1);
+    cursorDown();
+    char line2[13]= "Spring - 2022";
+    for(k=0;k<sizeof(line2);k++) writedata(line2[k],1);
+    cursorOff();
 
     return 0;
 }
